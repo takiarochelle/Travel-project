ef1acfad2b03583f7778d2db0f0016920716bba4
Takia Rudolph <takiarudolph@takias-mbp.attlocal.net>
2018-10-03 23:09:55 -0700

    Blocked login/ register ability while user is still logged in

--- server.py
+++ server.py
@@ -21,9 +21,28 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/register')
+def register():
+    """Display Registration page."""
+
+    email = session.get('email')
+
+    if email:
+        user = User.query.filter_by(email=email).first()
+        return redirect(url_for('user_profile', username=user.username))
+
+    return render_template('register.html')
+
+
 @app.route('/login')
 def login():
     """Display login page"""
+
+    email = session.get('email')
+    
+    if email:
+        user = User.query.filter_by(email=email).first()
+        return redirect(url_for('user_profile', username=user.username))
 
     return render_template('login.html')
 
@@ -45,12 +64,6 @@ def validate_login():
         flash('Email or password is incorrect')
         return redirect(url_for('login'))
 
-
-@app.route('/register')
-def register():
-    """Display Registration page."""
-
-    return render_template('register.html')
 
 
 @app.route('/validate-user', methods=['POST'])
@@ -96,19 +109,10 @@ def validate_new_user():
 def user_profile(username):
     """Display user's profile page"""
 
-    email = session.get('email')
-    user = User.query.filter_by(email=email).first()
-    user_fname = user.fname
-    created_trips = user.created_trips
-    other_trips = user.trips
-    profile_image = user.image_file
+    user = User.query.filter_by(username=username).first()
 
-    return render_template('profile.html',
-                            fname=user_fname, 
-                            created_trips=created_trips, 
-                            other_trips=other_trips, 
-                            username=username,
-                            profile_image=profile_image)
+    return render_template('profile.html', user=user)
+
 
 
 # @app.route('/my_submit', methods=['POST'])
@@ -139,11 +143,35 @@ def add_trip():
     return render_template('add_trip.html')
 
 
+@app.route('/<trip_name>-<trip_id>')
+def trip_itinerary(trip_name, trip_id):
+    
+    email = session.get('email')
+    place = request.args.get('place-name')
+    user = User.query.filter_by(email=email).first()
+    trip = db.session.query(Trip).get(trip_id)
+    place_exist = Place.query.filter_by(trip_id=trip_id, place_name=place).first()
+
+    if place and place_exist == None:
+        new_place = Place(trip_id=trip_id,
+                        user_id=user.user_id,
+                        place_name=place)
+
+        db.session.add(new_place)
+        db.session.commit()
+
+    return render_template('itinerary.html',
+                            trip_name=trip_name,
+                            trip_id=trip_id,
+                            trip=trip,
+                            username=user.username)
+
+
 @app.route('/validate-trip', methods=['POST'])
 def validate_trip():
 
-    user = User.query.filter_by(email=session['email']).first()
-
+    email = session.get('email')
+    user = User.query.filter_by(email=email).first()
     trip_name = request.form.get('trip-name')
     start_date = request.form.get('start-date')
     end_date = request.form.get('end-date')
@@ -160,47 +188,23 @@ def validate_trip():
 
     db.session.commit()
 
-    places = new_trip.places
-    travel_buddies = new_trip.travel_buddies
-
     return redirect(url_for('trip_itinerary',
-                            # YOUR_API_KEY=YOUR_API_KEY,
+                            trip_name=new_trip.trip_name, 
                             trip_id=new_trip.trip_id,
-                            trip_name=trip_name, 
-                            start_date=start_date,
-                            end_date=end_date,
-                            places=places,
-                            travel_buddies=travel_buddies))
+                            trip=new_trip,
+                            username=user.username))
 
 
-@app.route('/<trip_name>-<trip_id>')
-def trip_itinerary(trip_name, trip_id):
-    
-    email = session.get('email')
-    place = request.args.get('place-name')
-    user = User.query.filter_by(email=email).first()
-    trip = Trip.query.filter_by(trip_id=trip_id).first()
-    place_exist = Place.query.filter_by(trip_id=trip_id, place_name=place).first()
 
-    if place and place_exist == None:
-        new_place = Place(trip_id=trip_id,
-                        user_id=user.user_id,
-                        place_name=place)
+# @app.route('/delete-place.json')
+# def delete_place(list_of_places):
+#     """Remove place from the database"""
 
-        db.session.add(new_place)
-        db.session.commit()
+#     for place_id in list_of_places:
+#         place = Place.query.filter_by(place_id=place_id).first()
+#         place.delete()
 
-    places = trip.places
-    travel_buddies = trip.travel_buddies
-
-    return render_template('itinerary.html',
-                            # YOUR_API_KEY=YOUR_API_KEY, 
-                            trip_name=trip_name, 
-                            trip_id=trip_id,
-                            start_date=trip.start_date,
-                            end_date=trip.end_date,
-                            places=places,
-                            travel_buddies=travel_buddies)
+#     return 
 
 
 
--- static/main.js
+++ static/main.js
@@ -1,102 +1,131 @@
 'use strict';
 
-function initAutocomplete() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 51.507351, lng: -0.127758},
-      // center: getCurrentPosition();
-      zoom: 13,
-      mapTypeId: 'roadmap'
-});
+var map;
+var service;
+var infowindow;
 
+function initMap() {
+  var mapCenter = new google.maps.LatLng(-33.8617374,151.2021291);
 
-let infoWindow = new google.maps.InfoWindow({
-    width: 150
-});
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('You are here');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    }
-
-// Create the search box and link it to the UI element.
-// var input = document.getElementById('pac-input');
-var input = document.getElementById('search-place');
-var searchBox = new google.maps.places.SearchBox(input);
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-// Bias the SearchBox results towards current map's viewport.
-map.addListener('bounds_changed', function() {
-  searchBox.setBounds(map.getBounds());
-});
-
-var markers = [];
-// Listen for the event fired when the user selects a prediction and retrieve
-// more details for that place.
-searchBox.addListener('places_changed', function() {
-  var places = searchBox.getPlaces();
-
-  if (places.length == 0) {
-    return;
-  }
-
-  // Clear out the old markers.
-  markers.forEach(function(marker) {
-    marker.setMap(null);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: mapCenter,
+    zoom: 15
   });
-  markers = [];
 
-  // For each place, get the icon, name and location.
-  var bounds = new google.maps.LatLngBounds();
-  places.forEach(function(place) {
-    if (!place.geometry) {
-      console.log("Returned place contains no geometry");
-      return;
-    }
-    var icon = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
+  var request = {
+    query: 'Museum of Contemporary Art Australia',
+    fields: ['photos', 'formatted_address', 'name', 'rating', 'opening_hours', 'geometry'],
+  };
 
-    // Create a marker for each place.
-    markers.push(new google.maps.Marker({
-      map: map,
-      icon: icon,
-      title: place.name,
-      position: place.geometry.location
-    }));
-
-    if (place.geometry.viewport) {
-      // Only geocodes have viewport.
-      bounds.union(place.geometry.viewport);
-    } else {
-      bounds.extend(place.geometry.location);
-    }
-  });
-  map.fitBounds(bounds);
-});
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, callback);
 }
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
+// function initAutocomplete() {
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: 51.507351, lng: -0.127758},
+//       // center: getCurrentPosition();
+//       zoom: 13,
+//       mapTypeId: 'roadmap'
+// });
+
+
+// let infoWindow = new google.maps.InfoWindow({
+//     width: 150
+// });
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(function(position) {
+//         const pos = {
+//           lat: position.coords.latitude,
+//           lng: position.coords.longitude
+//         };
+
+//         infoWindow.setPosition(pos);
+//         infoWindow.setContent('You are here');
+//         infoWindow.open(map);
+//         map.setCenter(pos);
+//       }, function() {
+//         handleLocationError(true, infoWindow, map.getCenter());
+//       });
+//     } else {
+//       // Browser doesn't support Geolocation
+//       handleLocationError(false, infoWindow, map.getCenter());
+//     }
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//     infoWindow.setPosition(pos);
+//     infoWindow.setContent(browserHasGeolocation ?
+//                           'Error: The Geolocation service failed.' :
+//                           'Error: Your browser doesn\'t support geolocation.');
+//     }
+
+// // Create the search box and link it to the UI element.
+// // var input = document.getElementById('pac-input');
+// var input = document.getElementById('pac-input');
+// var searchBox = new google.maps.places.SearchBox(input);
+// map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+// // Bias the SearchBox results towards current map's viewport.
+// map.addListener('bounds_changed', function() {
+//   searchBox.setBounds(map.getBounds());
+// });
+
+// var markers = [];
+// // Listen for the event fired when the user selects a prediction and retrieve
+// // more details for that place.
+// searchBox.addListener('places_changed', function() {
+//   var places = searchBox.getPlaces();
+
+//   if (places.length == 0) {
+//     return;
+//   }
+
+//   // Clear out the old markers.
+//   markers.forEach(function(marker) {
+//     marker.setMap(null);
+//   });
+//   markers = [];
+
+//   // For each place, get the icon, name and location.
+//   var bounds = new google.maps.LatLngBounds();
+//   places.forEach(function(place) {
+//     if (!place.geometry) {
+//       console.log("Returned place contains no geometry");
+//       return;
+//     }
+//     var icon = {
+//       url: place.icon,
+//       size: new google.maps.Size(71, 71),
+//       origin: new google.maps.Point(0, 0),
+//       anchor: new google.maps.Point(17, 34),
+//       scaledSize: new google.maps.Size(25, 25)
+//     };
+
+//     // Create a marker for each place.
+//     markers.push(new google.maps.Marker({
+//       map: map,
+//       icon: icon,
+//       title: place.name,
+//       position: place.geometry.location
+//     }));
+
+//     if (place.geometry.viewport) {
+//       // Only geocodes have viewport.
+//       bounds.union(place.geometry.viewport);
+//     } else {
+//       bounds.extend(place.geometry.location);
+//     }
+//   });
+//   map.fitBounds(bounds);
+// });
+// }
 
 
 
