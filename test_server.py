821c23f1e2dfbb54432c2ffc8bf8e8ef40e42577
Takia Rudolph <takiaroc5@gmail.com>
2018-12-25 13:29:54 -0800

    Added test for validating login

--- test_server.py
+++ test_server.py
@@ -14,8 +14,18 @@ class FlaskIntegrationTestCase(unittest.TestCase):
 
     def test_index(self):
         """Test that homepage is rendering properly"""
+
         result = self.client.get('/')
         self.assertIn(b'<h1>Tripster</h1>', result.data)
+
+
+    def test_validate_login(self):
+        """Test that a successful login renders users profile page"""
+
+        result = self.client.post('/validate-login', data={'email': 'takia@gmail.com', 
+                                                            'password': 'abc123'}, 
+                                                            follow_redirects=True)
+        self.assertIn(b'Welcome, Takia', result.data)
 
 
 if __name__ == '__main__':
